ec5156ce3363533b7736894c707c10e482f39a60
zjnktion <zjnktion@163.com>
2018-06-09 09:55:38 +0800

    fix the HttpConnectionConfiguration bug

--- server/src/main/java/cn/zjnktion/his/server/service/DoctorService.java
+++ server/src/main/java/cn/zjnktion/his/server/service/DoctorService.java
@@ -2,6 +2,7 @@ package cn.zjnktion.his.server.service;
 
 import cn.zjnktion.his.server.model.Datagram;
 import cn.zjnktion.his.server.util.*;
+import com.fasterxml.jackson.core.type.TypeReference;
 import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
@@ -36,7 +37,6 @@ public class DoctorService {
     @RequestMapping(value = "/register", method = RequestMethod.POST)
     public ResponseBean<?> register(@RequestBody RequestBean<Object> requestBean, HttpServletRequest request) {
         Map<String, Object> params = requestBean.getParams();
-        jdbcTemplate.update("insert into t_user(account,password,name,operator_code,operator_psw,type,sex,card_num,phone,profession,nation,birthday,workday,education,learn_profession,practising_code,qualifications_code,work_code,create_time,update_time,status) values(？,？,？,？,？,？,？,？,？,？,？,？,？,？,？,？,？,?,now(),now(),1)", params.get("account"), params.get("password"), params.get("name"), params.get("operatorCode"), params.get("operatorPsw"), params.get("type"), params.get("sex"), params.get("cardNum"), params.get("phone"), params.get("profession"), params.get("nation"), params.get("birthday"), params.get("workday"), params.get("education"), params.get("learnProfession"), params.get("practisingCode"), params.get("qualificationsCode"), params.get("workCode"));
         Datagram req = requestBean.con2Basegram(request);
         // todo 换成登录用户的信息
         req.setOperatorCode("0001");
@@ -71,6 +71,54 @@ public class DoctorService {
         if (!"00000000".equalsIgnoreCase(resp.getTransReturnCode())) {
             return new ResponseBean<>(ErrorCode.ERROR);
         }
+        List<Map<String, String>> outputList = (List<Map<String,String>>) JsonUtil.json2Object(JsonUtil.getJsonString(resp.getTransBody().get("outputlist")), new TypeReference<List<Map<String, String>>>() {
+        });
+        jdbcTemplate.update("insert into t_user(account,password,name,operator_code,operator_psw,type,sex,card_num,phone,profession,nation,birthday,workday,education,learn_profession,practising_code,qualifications_code,work_code,create_time,update_time,status) values(？,？,？,？,？,？,？,？,？,？,？,？,？,？,？,？,？,?,now(),now(),1)", params.get("account"), params.get("password"), params.get("name"), outputList.get(0).get("bkc320"), "", params.get("type"), params.get("sex"), params.get("cardNum"), params.get("phone"), params.get("profession"), params.get("nation"), params.get("birthday"), params.get("workday"), params.get("education"), params.get("learnProfession"), params.get("practisingCode"), params.get("qualificationsCode"), params.get("workCode"));
+        ResponseBean<Map<String, Object>> responseBean = new ResponseBean<>(ErrorCode.SUCCESS);
+        responseBean.setData(resp.getTransBody());
+        return responseBean;
+    }
+
+    @Transactional(isolation = Isolation.REPEATABLE_READ, rollbackFor = Throwable.class)
+    @RequestMapping(value = "/update", method = RequestMethod.POST)
+    public ResponseBean<?> update(@RequestBody RequestBean<Object> requestBean, HttpServletRequest request) {
+        Map<String, Object> params = requestBean.getParams();
+        Datagram req = requestBean.con2Basegram(request);
+        // todo 换成登录用户的信息
+        req.setOperatorCode("0001");
+        req.setOperatorName("张三");
+        req.setOperatorPass(StringUtils.EMPTY);
+        req.getTransBody().put("listsize", 1);
+        List<Map<String, Object>> inputList = new ArrayList<>(1);
+        Map<String, Object> input = new HashMap<>(32);
+        input.put("bkc320", params.get("operatorCode"));
+        input.put("aac003", params.get("name"));
+        input.put("bka633", params.get("type"));
+        input.put("aac004", params.get("sex"));
+        input.put("aac058", "01");
+        input.put("aac147", params.get("cardNum"));
+        input.put("aae005", params.get("phone"));
+        input.put("bkc322", params.get("profession"));
+        input.put("aac005", params.get("nation"));
+        input.put("aac006", params.get("birthday"));
+        input.put("aac007", params.get("workday"));
+        input.put("aac011", params.get("education"));
+        input.put("aac183", params.get("learnProfession"));
+        input.put("bkc323", params.get("practisingCode"));
+        input.put("bke955", params.get("qualificationsCode"));
+        input.put("bkc324", "0");
+        input.put("acc501", params.get("workCode"));
+        input.put("bkc321", "1");
+        input.put("ckc302", (Integer) params.get("type") == 2 ? 1 : 0);
+        input.put("bkc325", (Integer) params.get("type") == 2 ? 1 : 0);
+        input.put("aae030", Integer.parseInt(DateFormatUtil.formatDate(new Date())));
+        inputList.add(input);
+        req.getTransBody().put("inputlist", JsonUtil.getJsonString(inputList));
+        Datagram resp = JsonUtil.json2Object(httpConnectionUtil.postWithJson(socialInsuranceUrl, JsonUtil.getJsonString(req)), Datagram.class);
+        if (!"00000000".equalsIgnoreCase(resp.getTransReturnCode())) {
+            return new ResponseBean<>(ErrorCode.ERROR);
+        }
+        jdbcTemplate.update("update t_user set name=?,operator_psw=?,type=?,sex=?,card_num=?,phone=?,profession=?,nation=?,birthday=?,workday=?,education=?,learn_profession=?,practising_code=?,qualifications_code=?,work_code=?,update_time=?,status=?", params.get("operatorPsw"), params.get("type"), params.get("sex"), params.get("cardNum"), params.get("phone"), params.get("profession"), params.get("nation"), params.get("birthday"), params.get("workday"), params.get("education"), params.get("learnProfession"), params.get("practisingCode"), params.get("qualificationsCode"), params.get("workCode"), new Date(), params.get("status"));
         ResponseBean<Map<String, Object>> responseBean = new ResponseBean<>(ErrorCode.SUCCESS);
         responseBean.setData(resp.getTransBody());
         return responseBean;
